9d87bb1763ad6428954f51fb28eb090bfe9b371d
arnokey123 <newarnold1@gmail.com>
2026-04-22 23:04:11 +0300

    Direct data fetch

--- hooks/useIntel.js
+++ hooks/useIntel.js
@@ -3,11 +3,14 @@ import useSWR from 'swr';
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function useIntel() {
-  // We point directly to the public file URL
-  // Replace 'ksi-dashboard-dusky' with your actual vercel app name if different
-  const { data, error, isLoading } = useSWR('https://ksi-dashboard-dusky.vercel.app/intel.json', fetcher, {
-    refreshInterval: 5000,
-  });
+  // Fetching directly from GitHub Raw file
+  const { data, error, isLoading } = useSWR(
+    'https://raw.githubusercontent.com/arnokey123/ksi-dashboard/master/public/intel.json', 
+    fetcher, 
+    {
+      refreshInterval: 5000,
+    }
+  );
 
   return {
     intelligenceData: data,
@@ -15,3 +18,4 @@ export default function useIntel() {
     isError: error,
   };
 }
+
