4206fcff93ce4f45e939657e7b81482ce21b8c16
arnokey123 <newarnold1@gmail.com>
2026-04-24 11:47:14 +0300

    Fixed Termux build crash

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // No special config needed here anymore
+
+
+
+
+
+typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors. Fixes the Termux build crash.
+    ignoreBuildErrors: true,
+  },
+
+
+
 };
 
 export default nextConfig;
